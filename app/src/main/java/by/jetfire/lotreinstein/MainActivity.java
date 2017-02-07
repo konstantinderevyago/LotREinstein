ab864acf650cac4f63ccdda3b8c54fa8dd14793c
Konstantin <jetfirer@gmail.com>
2017-02-07 12:14:50 +0300

    ViewPager and menu Items UI

--- app/src/main/java/by/jetfire/lotreinstein/MainActivity.java
+++ app/src/main/java/by/jetfire/lotreinstein/MainActivity.java
@@ -1,13 +1,59 @@
 package by.jetfire.lotreinstein;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    private MainPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        viewPager = (ViewPager) findViewById(R.id.main_pager);
+        tabLayout = (TabLayout) findViewById(R.id.main_tab);
+
+        setSupportActionBar(toolbar);
+        setTitle("");
+
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.task));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.conditions));
+
+        adapter = new MainPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.undo:
+                return true;
+            case R.id.redo:
+                return true;
+            case R.id.restart:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
