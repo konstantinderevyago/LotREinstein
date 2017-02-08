c008bf0e0d36f4ebd9b0ccacd0a2a750ff2fab1e
Konstantin <jetfirer@gmail.com>
2017-02-08 12:15:56 +0300

    Removed ViewPager

--- app/src/main/java/by/jetfire/lotreinstein/ui/activity/MainActivity.java
+++ app/src/main/java/by/jetfire/lotreinstein/ui/activity/MainActivity.java
@@ -1,43 +1,90 @@
 package by.jetfire.lotreinstein.ui.activity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-import by.jetfire.lotreinstein.ui.adapter.MainPagerAdapter;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import by.jetfire.lotreinstein.R;
+import by.jetfire.lotreinstein.entity.TaskItem;
+import by.jetfire.lotreinstein.utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
+    @BindView(R.id.main_container)
+    protected LinearLayout container;
 
-    private MainPagerAdapter adapter;
+    private List<TaskItem> taskItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
-        viewPager = (ViewPager) findViewById(R.id.main_pager);
-        tabLayout = (TabLayout) findViewById(R.id.main_tab);
+        taskItems = Utils.getTaskItems(this);
+        buildTable(container);
+    }
 
-        setSupportActionBar(toolbar);
-        setTitle("");
+    private void buildTable(ViewGroup view) {
+        if (taskItems != null && !taskItems.isEmpty()) {
+            buildTitleColumn(view);
+            for (TaskItem taskItem : taskItems) {
+                buildColumn(view, taskItem.getNumber());
+            }
+        }
+    }
 
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.task));
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.conditions));
+    private void buildTitleColumn(ViewGroup view) {
+        LinearLayout linearLayout = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.column_item, view, false);
+        buildTextView(linearLayout, R.string.number);
+        buildTextView(linearLayout, R.string.name);
+        buildTextView(linearLayout, R.string.color);
+        buildTextView(linearLayout, R.string.alcohol);
+        buildTextView(linearLayout, R.string.smoke);
+        buildTextView(linearLayout, R.string.thing);
+        view.addView(linearLayout);
+    }
 
-        adapter = new MainPagerAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(adapter);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+    private void buildColumn(ViewGroup view, int number) {
+        LinearLayout linearLayout = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.column_item, view, false);
+        buildTextView(linearLayout, String.valueOf(number));
+        buildSpinner(linearLayout, R.array.names);
+        buildSpinner(linearLayout, R.array.colors);
+        buildSpinner(linearLayout, R.array.alcohols);
+        buildSpinner(linearLayout, R.array.smokes);
+        buildSpinner(linearLayout, R.array.things);
+        view.addView(linearLayout);
+    }
+
+    private void buildTextView(ViewGroup view, int textRes) {
+        buildTextView(view, getString(textRes));
+    }
+
+    private void buildTextView(ViewGroup view, String text) {
+        TextView textView = (TextView) LayoutInflater.from(this).inflate(R.layout.text_item, view, false);
+        textView.setText(text);
+        view.addView(textView);
+    }
+
+    private void buildSpinner(ViewGroup view, int arrayRes) {
+        Spinner spinner = (Spinner) LayoutInflater.from(this).inflate(R.layout.spinner_item, view, false);
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(arrayRes));
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerArrayAdapter);
+        view.addView(spinner);
     }
 
     @Override
@@ -50,13 +97,65 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.undo:
+                undo();
                 return true;
             case R.id.redo:
+                redo();
+                return true;
+            case R.id.conditions:
+                showConditionsDialog();
                 return true;
             case R.id.restart:
+                showRestartDialog();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void undo() {
+
+    }
+
+    private void redo() {
+
+    }
+
+    private void showConditionsDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.conditions)
+                .setMessage(getConditions())
+                .show();
+    }
+
+    private String getConditions() {
+        return getString(R.string.condition_1) + "\n" +
+                getString(R.string.condition_2) + "\n" +
+                getString(R.string.condition_3) + "\n" +
+                getString(R.string.condition_4) + "\n" +
+                getString(R.string.condition_5) + "\n" +
+                getString(R.string.condition_6) + "\n" +
+                getString(R.string.condition_7) + "\n" +
+                getString(R.string.condition_8) + "\n" +
+                getString(R.string.condition_9) + "\n" +
+                getString(R.string.condition_10) + "\n" +
+                getString(R.string.condition_11) + "\n" +
+                getString(R.string.condition_12) + "\n" +
+                getString(R.string.condition_13) + "\n" +
+                getString(R.string.condition_14) + "\n" +
+                getString(R.string.condition_15);
+    }
+
+    private void showRestartDialog() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.restart_message)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
     }
 }
